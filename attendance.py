61953d1c15299bc34578a16ba1709c735055d9cf
Alexander Martin <alex@alex-macbookpro.attlocal.net>
2016-11-13 14:22:03 -0500

    Merged Attendance & Registration with Commandline Arguments Now supports -o for offline use, and -r to register new users. It defaults to online use and attendance. I still plan to add a flag to change a user, but I don't know how I'd do that on the google sheet side. I also need to refractor the code, some of this stuff could be made into functions.

--- attendance.py
+++ attendance.py
@@ -1,18 +1,45 @@
 #!/usr/bin/env python
-import sys
-import serial
-import csv
-import time
-import datetime
-import requests
+import sys, serial, csv, time, requests, glob, yaml, argparse, datetime
 
-s = serial.Serial('/dev/cu.usbmodem1411', 9600, timeout=1)
-day = datetime.date.today().strftime("%m-%d-%y")
-print(s.name)
+#Parse the arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("-o", "--offline", help="run in offline mode", action="store_true")
+parser.add_argument("-r", "--register", help="register new users", action="store_true")
+args = parser.parse_args()
+online = not(args.offline)
 
-attendanceForm = "1TyUw_tnk94NYrvrTONOZ2WxfCVtwljMd27GlTs-7r9w"
-attendanceID = "entry.945552943"
+#read from config
+with open("config.yml", 'r') as config:
+	try:
+		configData = yaml.safe_load(config)
+	except yaml.YAMLError as e:
+		sys.stdout.write("Failed to parse config file.\n")
+		print e
+		sys.exit(1)
 
+setupForm = configData["registration"]["form"]
+setupName = configData["registration"]["name"]
+setupID   = configData["registration"]["id"]
+
+attendanceForm = configData["attendance"]["form"]
+attendanceID   = configData["attendance"]["id"]
+
+#Connect to Arduino
+ports = glob.glob("/dev/tty.usbmodem*")
+connected = False
+for port in ports:
+	if(not connected):
+		try:
+			s = serial.Serial(port, 9600, timeout=1)
+			connected = True
+		except:
+			connected = False
+if(not connected):
+	print("Failed to autodetect serial port.")
+	sys.exit()
+print("Connected on " + s.name)
+
+#Wait for Arduino
 ready = False
 print("Waiting...")
 while(not ready):
@@ -20,35 +47,88 @@ while(not ready):
 	if("Ready" in read):
 		ready = True
 
-sys.stdout.write("Ready to take attendance.\n\n")
+if(args.register):
+	sys.stdout.write("Ready to register ID tags.")
+else:
+	sys.stdout.write("Ready to take attendance.")
+sys.stdout.write("\nID: ")
 sys.stdout.flush()
+
+day = datetime.date.today().strftime("%m-%d-%y")
+lastScanTime = datetime.datetime.now()
+minScanTime = datetime.timedelta(seconds=1) 
+
+#Main Loop
 try:
 	while(True):
 		read = s.readline()
 		if("ID: " in read):
-			id = read[4:].rstrip()
-			with open("database.csv", 'rb') as file:
-				contents = file.read()
-				if(id not in contents):
-					sys.stdout.write(id + "\nName: ")
-					sys.stdout.write("Error, ID not found. Try scanning again, or if you are not registered, run the setup script.\n\n")
-				else:
-					for line in contents.splitlines():
-						if(id in line):
-							try:
-								data = line.split("\"")
-								sys.stdout.write("Welcome, " + data[3] + "!\n\n")
-								sys.stdout.flush()
-								break;
-							except IndexError:
-								sys.stdout.write("Welcome, User!\n\n")
-								break
-			
-			requestString = "https://docs.google.com/forms/d/" + attendanceForm + "/formResponse?ifq&" + attendanceID + "=" + id
-			requests.get(requestString)
+			currentScanTime = datetime.datetime.now()
+			if(currentScanTime - lastScanTime > minScanTime):
+				id = read[4:].rstrip()
+				sys.stdout.write(id + "\n")
+				with open("database.csv", 'rb') as file:
+					if(args.register):
+						contents = file.read()
+						if(id not in contents):
+							sys.stdout.write("Name: ")
+							sys.stdout.flush()
+							name = sys.stdin.readline().rstrip()
+						
+							if(online):
+								try:
+									request1String = "https://docs.google.com/forms/d/" + setupForm + "/formResponse?ifq&" + setupID + "=" + id + "&" + setupName + "=" + name
+									request2String = "https://docs.google.com/forms/d/" + attendanceForm + "/formResponse?ifq&" + attendanceID + "=" + id
+				
+									requests.get(request1String)
+									requests.get(request2String)
+								except requests.exceptions.ConnectionError:
+									online = False
+									sys.stdout.write("\nCould not connect to the internet, running in offline mode.\n")
 	
-			with open(day+".csv", 'a') as outFile:
-				outFile.write("\n\""+ id + "\"")
+							with open("database.csv", 'a') as outFile:
+								outFile.write("\n\""+ id + "\",\"" + name + "\"")
+			
+							sys.stdout.write("\nID: ")
+							sys.stdout.flush()
+						else:
+							for line in contents.splitlines():
+								if(id in line):
+									data = line.split("\"")
+									sys.stdout.write(data[1] + " is already registered to " + data[3] + "\n\nID: ")
+									sys.stdout.flush()
+					else:
+						contents=file.read()
+						if(id not in contents):
+							sys.stdout.write("Error, ID not found. Try scanning again, or if you are not registered, run the setup script.\n\nID: ")
+							sys.stdout.flush()
+						else:
+							for line in contents.splitlines():
+								if(id in line):
+									try:
+										data = line.split("\"")
+										sys.stdout.write("Welcome, " + data[3] + "!\n\n")
+										sys.stdout.flush()
+										break
+									except IndexError:
+										sys.stdout.write("Welcome, User!\n\n")
+										sys.stdout.flush()
+										break
+							if(online):
+								try:
+									requests.get("https://docs.google.com/forms/d/" + attendanceForm + "/formResponse?ifq&" + attendanceID + "=" + id)
+								except requests.exceptions.ConnectionError:
+									online = False
+									sys.stdout.write("\nCould not connect to the internet, running in offline mode.\n")
+									with open(day+".csv", 'a') as outFile:
+										outFile.write("\n\""+ id + "\"")
+							else:
+								with open(day+".csv", 'a') as outFile:
+									outFile.write("\n\""+ id + "\"")
+							sys.stdout.write("ID: ")
+							sys.stdout.flush()
+			lastScanTime = datetime.datetime.now()
 except KeyboardInterrupt:
-	sys.stdout.write("\n\nGoodbye!\n")
+	sys.stdout.write("\n\nDone!\n")
+	sys.stdout.flush
 	sys.exit()
